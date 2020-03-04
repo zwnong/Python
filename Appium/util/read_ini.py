25148bd270bd78b89ea7ee7350f454b72284cbf3
nongzhongwen <18778600955@163.com>
2020-03-04 20:47:38 +0800

    updateSomethings202003044738

---
+++ Appium/util/read_ini.py
@@ -0,0 +1,32 @@
+# coding = utf-8
+
+import configparser
+
+
+# conf = configparser.ConfigParser()
+# conf.read(r'D:\Git\Learnning-Python\Appium\config\settingsElements.ini', encoding='UTF-8')
+# print(conf.get('settingsElements', 'FlymeAccount'))
+
+
+class ReadIni:
+    def __init__(self, file_path=None):
+        if file_path is None:
+            self.file_path = r'D:\Git\Learnning-Python\Appium\config\settingsElements.ini'
+        else:
+            self.file_path = file_path
+        self.data = self.read_ini()
+
+    def read_ini(self):
+        conf = configparser.ConfigParser()
+        conf.read(self.file_path, encoding='UTF-8')
+        return conf
+
+    # 通过传入key，获取value
+    def get_value(self, key, section):
+        value = self.data.get(self, key)
+        return value
+
+
+if __name__ == '__main__':
+    read_ini = ReadIni()
+    print(read_ini.get_value('settingsElements', 'network'))
