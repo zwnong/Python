4bdb81d91974c5f6ae4f6f82715c9282d6f7d62a
zwnong <18778600955@163.com>
2020-03-08 12:49:54 +0800

    updateSomethings202003084953

--- Appium/util/get_ini_value.py
+++ Appium/util/get_ini_value.py
@@ -1,6 +1,30 @@
 # coding = utf-8
+import sys
+sys.path.append('D:\git\Python\Appium')
 from util.read_ini import ReadIni
 
 class GetIniValue:
-    def get_ini_value(self):
-        pass
+    
+    def __init__(self,driver):
+        self.driver = driver
+        
+    def get_ini_value(self,key):
+        read_ini = ReadIni()
+        value = read_ini.get_value(key)
+        by = value.split('>')[0]
+        value_by = value.split('>')[0]
+        
+        if value != None:
+            if by == 'id':
+                return self.driver.find_element_by_id(value_by)
+            
+            elif by == 'className':
+                return self.driver.find_element_by_class_name(value_by)
+            
+            else:
+                return None
+        
+
+if __name__ == '__main__':
+    value = GetIniValue()
+    value.get_ini_value('FlymeAccount')
