a450ebeb5e99c41f2e1ed26fd601a0bcaa781b1b
zwnong <18778600955@163.com>
2020-03-09 14:52:38 +0800

    updateSomethings202003095238

--- scripts/git.bat
+++ scripts/git.bat
@@ -1,9 +1,9 @@
 @echo off
 echo github uploading...
 d:
-cd Git\Learnning-Python
+cd D:\Git\Python
 git add -A
 set a=%date:~0,4%%date:~5,2%%date:~8,2%%h%%time:~3,2%%time:~6,2%
 git commit -a -m "updateSomethings%a%"
-git push origin appium
+git push origin master
 pause
