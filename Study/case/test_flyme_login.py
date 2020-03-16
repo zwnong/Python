70a505b4f271a866f1a30231480cd595eb2ed54c
zwnong <18778600955@163.com>
2020-03-16 20:26:56 +0800

    updateSomethings202003162656

--- Study/case/test_flyme_login.py
+++ Study/case/test_flyme_login.py
@@ -5,32 +5,31 @@ import time
 import unittest
 from appium import webdriver
 import HTMLTestRunner
-from business.flyme_login_business import LoginBusiness
 
 
 class TestFlyMeLoginFunc(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.flyme_login_business = LoginBusiness()
+        pass
 
     def setUp(self):
         capabilities = {
             "platformName": "Android",
-            # "automationName":"UiAutomator2",
-            "deviceName": "127.0.0.1:21503",
-            "app": "com.android.settings",
-            "appWaitActivity": "com.android.settings.Settings",
-            "noReset": "true"
+            "automationName": "UiAutomator2",
+            "deviceName": "973QAFV33GVJN",
+            "app": r"E:\APK\meizustore.apk",
+            "appWaitActivity": "com.meizu.store.newhome.NewHomeActivity",
+            "noReset": "True"
         }
         self.driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", capabilities)
-        time.sleep(10)
+        time.sleep(5)
         return self.driver
 
     def tearDown(self):
         self.driver.quit()
 
     def test_01(self):
-        self.flyme_login_business.flyme_login_pass()
+        self.driver.press_keycode(3)
 
     @staticmethod
     def case_suite():
@@ -41,7 +40,7 @@ class TestFlyMeLoginFunc(unittest.TestCase):
         # runner = unittest.TextTestRunner()
         # runner.run(suite)
         runner = HTMLTestRunner.HTMLTestRunner()
-        file_path = r'D:\Git\Python\Appium\report\report' + now + '.html'
+        file_path = r'D:\Git\Python\Study\report\report' + now + '.html'
         with open(file_path, 'wb') as f:
             HTMLTestRunner.HTMLTestRunner(stream=f, title='测试报告', description='描述:').run(suite)
 
