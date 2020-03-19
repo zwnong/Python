9c37b9f59fc62658dddaa8a386bc326a268eaf84
zwnong <18778600955@163.com>
2020-03-19 14:59:34 +0800

    updateSomethings202003195934

--- Tkinter/tools/monkey.py
+++ Tkinter/tools/monkey.py
@@ -17,18 +17,12 @@ window.geometry("400x400+200+50")
 # 封装button1的功能,执行adb shell monkey -p com.meizu.filemanager -s 1000 --pct-touch 35 --pct-motion 25 --pct-appswitch 20
 # --pct-nav 10 --pct-majornav 10 --throttle 1000 --monitor-native-crashes -v -v 10000 > E:\monkeylog\camrea.txt
 def fnc():
-    os.popen('adb shell rm -rf sdcard/log1')
-    time.sleep(1)
-    os.popen('adb shell mkdir sdcard/log1')
-    time.sleep(1)
-    os.popen('adb shell touch sdcard/log1/log.txt')
-    log = 'sdcard/log1/log.txt'
-
+    os.popen('adb shell touch sdcard/log.txt')
     package = entry.get()
-    nub = entry1.get()
+    numb = entry1.get()
 
     os.popen('adb shell monkey -p ' + package + ' -s 1000 --pct-touch 35 --pct-motion 25 --pct-appswitch 20 --pct-nav 10 --pct-majornav 10 --throttle '
-                                                '10 --monitor-native-crashes -v -v ' + str(nub) + log)
+                                                '10 --monitor-native-crashes -v -v ' + numb + '>E:\log.txt')
 
 
 label = tkinter.Label(window, text="请输入包名", justify="left")
