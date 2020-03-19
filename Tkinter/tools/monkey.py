109dd9c0c2a147b3538b0e69ea763f371281b8e3
zwnong <18778600955@163.com>
2020-03-19 17:16:56 +0800

    updateSomethings202003191656

--- Tkinter/tools/monkey.py
+++ Tkinter/tools/monkey.py
@@ -6,7 +6,7 @@ import tkinter
 # 创建窗体
 window = tkinter.Tk()
 window.title('monkey')
-window.geometry("400x400+200+50")
+window.geometry("400x200+200+50")
 
 """
 1、获取输入框输入的包名（判断是否为真）
@@ -14,15 +14,25 @@ window.geometry("400x400+200+50")
 """
 
 
+def get_package_vlue():
+    result = os.popen('adb shell dumpsys window | findstr mCurrentFocus').readline()
+    s = result.split(':')[1]
+    a = s.split('/')
+    b = a[0]
+    print(b)
+    numb = entry1.get()
+    print(numb)
+    os.popen('adb shell monkey -p ' + b + ' -s 1000 --pct-touch 35 --pct-motion 25 --pct-appswitch 20 --pct-nav 10 --pct-majornav 10 --throttle '
+                                          '600 --monitor-native-crashes -v -v ' + numb + '>E:\log.txt')
+
+
 # 封装button1的功能,执行adb shell monkey -p com.meizu.filemanager -s 1000 --pct-touch 35 --pct-motion 25 --pct-appswitch 20
 # --pct-nav 10 --pct-majornav 10 --throttle 1000 --monitor-native-crashes -v -v 10000 > E:\monkeylog\camrea.txt
 def fnc():
-    os.popen('adb shell touch sdcard/log.txt')
     package = entry.get()
     numb = entry1.get()
-
     os.popen('adb shell monkey -p ' + package + ' -s 1000 --pct-touch 35 --pct-motion 25 --pct-appswitch 20 --pct-nav 10 --pct-majornav 10 --throttle '
-                                                '10 --monitor-native-crashes -v -v ' + numb + '>E:\log.txt')
+                                                '600 --monitor-native-crashes -v -v ' + numb + '>E:\log.txt')
 
 
 label = tkinter.Label(window, text="请输入包名", justify="left")
@@ -39,8 +49,12 @@ label.pack()
 entry1 = tkinter.Entry(window)
 entry1.pack()
 
-# 创建按钮
-button1 = tkinter.Button(window, text="运行", command=fnc, width=20, height=6)
+# 创建按钮  需要输入包名和点击次数
+button1 = tkinter.Button(window, text="运行", command=fnc, width=19, height=1)
 button1.pack()
+
+# 输入随机数，运行当前应用
+button2 = tkinter.Button(window, text="输入次数 运行当前应用", command=get_package_vlue, width=19, height=1)
+button2.pack()
 
 window.mainloop()
