4b54b62808fee096500c18bb6eaa4c4eeb197845
zwnong <18778600955@163.com>
2020-03-20 15:51:08 +0800

    updateSomethings202003205108

--- Tkinter/tools/monkey.py
+++ Tkinter/tools/monkey.py
@@ -14,7 +14,7 @@ window.geometry("400x200+200+50")
 """
 
 
-def get_package_vlue():
+def button_two():
     result = os.popen('adb shell dumpsys window | findstr mCurrentFocus').readline()
     print(result)
     a = result.split('u0')[1]
@@ -23,7 +23,7 @@ def get_package_vlue():
     numb = entry1.get()
     print(numb)
     os.popen('adb shell monkey -p ' + package_name + ' -s 1000 --pct-touch 35 --pct-motion 25 --pct-appswitch 20 --pct-nav 10 --pct-majornav 10 --throttle '
-                                                     '600 --monitor-native-crashes -v -v ' + numb + '>E:\log.txt')
+                                                     '800 --monitor-native-crashes -v -v ' + numb + '>E:\log.txt')
 
 
 # 封装button1的功能,执行adb shell monkey -p com.meizu.filemanager -s 1000 --pct-touch 35 --pct-motion 25 --pct-appswitch 20
@@ -32,20 +32,22 @@ def fnc():
     package = entry.get()
     numb = entry1.get()
     os.popen('adb shell monkey -p ' + package + ' -s 1000 --pct-touch 35 --pct-motion 25 --pct-appswitch 20 --pct-nav 10 --pct-majornav 10 --throttle '
-                                                '600 --monitor-native-crashes -v -v ' + numb + '>E:\log.txt')
+                                                '800 --monitor-native-crashes -v -v ' + numb + '>E:\log.txt')
 
-
+# 输入包名提示
 label = tkinter.Label(window, text="请输入包名", justify="left")
 label.pack()
 
 # 创建输入框
+# 包名输入框
 entry = tkinter.Entry(window)
 entry.pack()
 
-# 输入次数框
+# 提示输入次数框
 label = tkinter.Label(window, text="请输入随机点击次数", justify="left")
 label.pack()
 
+# 输入次数框
 entry1 = tkinter.Entry(window)
 entry1.pack()
 
@@ -54,7 +56,7 @@ button1 = tkinter.Button(window, text="运行", command=fnc, width=19, height=1)
 button1.pack()
 
 # 输入随机数，运行当前应用
-button2 = tkinter.Button(window, text="输入次数 运行当前应用", command=get_package_vlue, width=19, height=1)
+button2 = tkinter.Button(window, text="输入次数 运行当前应用", command=button_two, width=19, height=1)
 button2.pack()
 
 window.mainloop()
